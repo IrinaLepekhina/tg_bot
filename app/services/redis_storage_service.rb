059fef5a59ac608fe4b4085fe1238db81287b7fe
Irina Lepekhina <lepekhina-irina@yandex.ru>
2023-11-13 19:06:00 -0600

    refactor: Renamed Redis variables for better clarity

--- app/services/redis_storage_service.rb
+++ app/services/redis_storage_service.rb
@@ -1,18 +1,15 @@
 # app/services/redis_storage_service.rb
-
 class RedisStorageService
   include Loggable
-
-  def initialize(language_service = nil)
+  def initialize
     begin
-      @redis = Redis.new(host: "#{ENV['REDIS_HOST_BOT']}", port: "#{ENV['REDIS_PORT_BOT']}".to_i)
+      @redis = Redis.new(host: "#{ENV['REDIS_HOST']}", port: "#{ENV['REDIS_PORT']}".to_i)
       log_info("Successfully initialized Redis connection")
     rescue => e
       log_exception(e)
       raise e
     end
   end
-
   def client
     @redis
   end
